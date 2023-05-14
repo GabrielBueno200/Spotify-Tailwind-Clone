92cac7f517771f96a9f3a369d24d4d4bccbc87d2
GabrielBueno200 <gabrielbueno200@gmail.com>
2023-05-14 20:34:18 -0300

    fixed footer player fill

--- src/app/components/Player/index.tsx
+++ src/app/components/Player/index.tsx
@@ -38,7 +38,7 @@ const Player = () => (
         <SkipBack size={20} className="text-zinc-200" />
 
         <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-zinc-950 fill-zinc-950">
-          <Play />
+          <Play className="fill-zinc-950" />
         </button>
 
         <SkipForward size={20} className="text-zinc-200" />
