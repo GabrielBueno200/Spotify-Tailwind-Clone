d906ad7e21dbb384b7c16d5aff0e63b969cd335c
GabrielBueno200 <gabrielbueno200@gmail.com>
2023-05-14 13:47:37 -0300

    updated sidebar width

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -2,7 +2,7 @@ import OptionsNav from "../OptionsNav";
 import LibraryNav from "../LibraryNav";
 
 const Sidebar = () =>
-    <aside className="w-80 bg-zinc-950 p-2 pb-0 flex flex-col gap-3">
+    <aside className="w-96 bg-zinc-950 p-2 pb-0 flex flex-col gap-3">
         <OptionsNav />
         <LibraryNav />
     </aside>
